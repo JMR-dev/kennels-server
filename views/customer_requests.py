5ff224c2d2e87e97df04c2ef141f4b6474358823
bytesparallel <jason.ross841@gmail.com>
2022-02-03 10:32:20 -0600

    saving refactors to get methods to use SQLite instead of hard-coded data

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -1,45 +1,44 @@
-CUSTOMERS = [
-    {
-      "id": 1,
-      "name": "Ryan Tanay",
-      "email": "ryan@tanay.com",
-    },
-    {
-      "id": 2,
-      "name": "Emma Beaton",
-      "email": "emma@beaton.com",
-   
-    },
-    {
-      "id": 3,
-      "name": "Dani Adkins",
-      "email": "dani.adkins.com",
-    },
-    {
-      "id": 4,
-      "name": "Adam Oswalt",
-      "email": "adam@oswalt.com",
-    },
-    {
-      "id": 5,
-      "name": "Fletcher Bangs",
-      "email": "flangs@bangs.com",
-    },
-    {
-      "id": 6,
-      "name": "Angela Lee",
-      "email": "lee@lee.com",
-    },
-    {
-      "name": "mike mike",
-      "email": "m@m.com",
-      "id": 7
-    },
-
-             ]
-
 def get_all_customers():
-    return CUSTOMERS
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password,
+        FROM animal a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        animals = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+
+            animals.append(animal.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(animals)
+
 
 def get_single_customer():
     requested_customer = None
